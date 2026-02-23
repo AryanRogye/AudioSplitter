d75ab64a6ab67d345150a532c5d3059912d7e0b5
AryanRogye <aryan.rogye@gmail.com>
2026-02-23 12:16:22 -0600

    migrating

---
+++ ComfyAudio/ComfyAudio/ComfyAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComfyAudioApp.swift
+//  ComfyAudio
+//
+//  Created by Aryan Rogye on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ComfyAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
